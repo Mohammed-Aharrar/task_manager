f6d5e369c4a6c3fe18dca0f4f27b7fd13e903e33
Mohammed-Aharrar <mohamedaharrar7@gmail.com>
2024-12-10 04:30:02 +0100

    Added automatic static code analysis execution

--- cli.py
+++ cli.py
@@ -3,27 +3,31 @@ from tasks.manager import TaskManager
 
 manager = TaskManager()
 
+
 @click.group()
 def cli():
     """Task Management System"""
     pass
 
+
 @cli.command()
-@click.option('--title', prompt='Title', help='Title of the task')
-@click.option('--description', prompt='Description', help='Description of the task')
-@click.option('--due_date', prompt='Due Date', help='Due date of the task')
+@click.option("--title", prompt="Title", help="Title of the task")
+@click.option("--description", prompt="Description", help="Description of the task")
+@click.option("--due_date", prompt="Due Date", help="Due date of the task")
 def add(title, description, due_date):
     """Add a new task"""
     manager.add_task(title, description, due_date)
     click.echo(f"Task '{title}' added!")
+
 
 @cli.command()
 def list():
     """List all tasks"""
     manager.list_tasks()
 
+
 @cli.command()
-@click.argument('task_id', type=int)
+@click.argument("task_id", type=int)
 def complete(task_id):
     """Mark a task as complete"""
     if manager.mark_complete(task_id):
@@ -31,8 +35,9 @@ def complete(task_id):
     else:
         click.echo("Task not found.")
 
+
 @cli.command()
-@click.argument('task_id', type=int)
+@click.argument("task_id", type=int)
 def remove(task_id):
     """Remove a task"""
     if manager.remove_task(task_id):
@@ -40,5 +45,6 @@ def remove(task_id):
     else:
         click.echo("Task not found.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
